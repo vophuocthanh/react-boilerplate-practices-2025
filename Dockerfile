f0d751bb8f78c4186eb4f18c84a215ad21c193ce
vophuocthanh <phuocthanh2k03@gmail.com>
2025-11-12 13:07:40 +0700

    feat: add Dockerfile for application containerization and configure CI workflow

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:22-alpine
+
+WORKDIR /app
+
+COPY package.json yarn.lock ./
+
+RUN yarn install
+
+COPY . .
+
+CMD ["yarn", "dev"]
+
+EXPOSE 4000
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
     port: 4000,
     open: true
   },
